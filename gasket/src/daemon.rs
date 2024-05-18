3eeb7aff67b58e71a138ad8f1371eb338ae08dbf
Santiago Carmuega <santiago@carmuega.me>
2024-05-18 08:17:34 -0300

    feat: handle OS term signals

--- gasket/src/daemon.rs
+++ gasket/src/daemon.rs
@@ -1,18 +1,39 @@
-use std::time::Duration;
+use std::{
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
+    time::Duration,
+};
 
-use tracing::info;
+use tracing::{info, warn};
 
 use crate::runtime::{StagePhase, Tether, TetherState};
 
 #[derive(Debug)]
-pub struct Daemon(pub Vec<Tether>);
+pub struct Daemon(Vec<Tether>, Arc<AtomicBool>);
 
 impl Daemon {
+    pub fn new(tethers: Vec<Tether>) -> Self {
+        let term = Arc::new(AtomicBool::new(false));
+
+        for sig in signal_hook::consts::TERM_SIGNALS {
+            signal_hook::flag::register(*sig, Arc::clone(&term))
+                .expect("can't register OS signal hook");
+        }
+
+        Self(tethers, term)
+    }
+
     pub fn tethers(&self) -> impl Iterator<Item = &Tether> {
         self.0.iter()
     }
 
-    pub fn should_stop(&self) -> bool {
+    pub fn is_terminated(&self) -> bool {
+        self.1.load(Ordering::Relaxed)
+    }
+
+    pub fn has_ended(&self) -> bool {
         self.0.iter().any(|tether| match tether.check_state() {
             TetherState::Alive(p) => {
                 matches!(p, StagePhase::Ended)
@@ -21,25 +42,36 @@ impl Daemon {
         })
     }
 
-    pub fn teardown(&self) {
+    pub fn should_stop(&self) -> bool {
+        if self.is_terminated() {
+            warn!("daemon terminated by user");
+            return true;
+        }
+
+        if self.has_ended() {
+            warn!("pipeline ended or stalled");
+            return true;
+        }
+
+        false
+    }
+
+    pub fn teardown(self) {
+        // first pass is to notify that we should stop
         for tether in self.0.iter() {
             let state = tether.check_state();
             info!(stage = tether.name(), ?state, "dismissing stage");
             tether.dismiss_stage().expect("stage stops");
+        }
 
-            // Can't join the stage because there's a risk of deadlock, usually
-            // because a stage gets stuck sending into a port which depends on a
-            // different stage not yet dismissed. The solution is to either
-            // create a DAG of dependencies and dismiss in the
-            // correct order, or implement a 2-phase teardown where
-            // ports are disconnected and flushed before joining the
-            // stage.
-
-            //tether.join_stage();
+        // second pass is to wait for graceful shutdown
+        info!("waiting for stages to end");
+        for tether in self.0.into_iter() {
+            tether.join_stage();
         }
     }
 
-    pub fn block(&self) {
+    pub fn block(self) {
         while !self.should_stop() {
             std::thread::sleep(Duration::from_millis(1500));
         }
